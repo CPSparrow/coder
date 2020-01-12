e97c54b413ae2d11496e319abf430dbc210228ee
Old Wan <36687023+CPSparrow@users.noreply.github.com>
2020-01-12 11:37:53 +0800

    Add files via upload

---
+++ coder.h
@@ -0,0 +1,20 @@
+//  coder.h
+//  copyright @ Handsome Coder Studio, 2020
+//  maybe this can describe what happened to me
+//  I really hope Mommy
+
+void eat(void);
+
+void sleep(void);
+
+void code(void);
+
+void life(void){
+	eat();
+	sleep();
+	code();
+}
+
+void code(void){
+	code();
+}
