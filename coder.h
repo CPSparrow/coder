11927f45081ce7de3591b36b1190aca3a20fe0ac
Old Wan <36687023+CPSparrow@users.noreply.github.com>
2020-01-12 11:39:03 +0800

    Update coder.h

--- coder.h
+++ coder.h
@@ -1,7 +1,7 @@
 //  coder.h
 //  copyright @ Handsome Coder Studio, 2020
 //  maybe this can describe what happened to me
-//  I really hope Mommy
+//  I really hope Mommy would understand
 
 void eat(void);
 
